3bbdf53f3e5fbcd4a86f6341625d1f9b9e6ff4a0
Jaume Alavedra <jamalavedra@gmail.com>
2025-10-25 18:29:34 +0100

    chore: update endpoint url

--- pages/api/protected-create-encryption-session.ts
+++ pages/api/protected-create-encryption-session.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
+
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
